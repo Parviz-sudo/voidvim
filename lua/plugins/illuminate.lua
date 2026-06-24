f10b2ecbe37a72e5e649a7549dd5e3d381cb52c0
Parviz <mparviz217@proton.me>
2026-06-24 14:33:15 +0330

    fix: avoid nvim-treesitter locals.lua bug in vim-illuminate

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -6,6 +6,10 @@ return {
 		config = function()
 			require("illuminate").configure({
 				delay = 200,
+				-- the "treesitter" provider can trigger a known nvim-treesitter
+				-- locals.lua bug ("attempt to call method 'parent' (a nil value)")
+				-- on some files; lsp + regex cover the same use case safely.
+				providers = { "lsp", "regex" },
 			})
 		end,
 	},
