d259cdfec3a2dfdb62dc83bd6f96c289bd5a3edb
Parviz <mparviz217@proton.me>
2026-06-28 19:59:07 +0330

    fix(theme): remove transparency, switch to tokyonight night style

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,44 +1,23 @@
-local function enable_transparency()
-	local groups = {
-		"Normal",
-		"NormalNC",
-		"NormalFloat",
-		"FloatBorder",
-		"SignColumn",
-		"MsgArea",
-		"EndOfBuffer",
-	}
-	for _, group in ipairs(groups) do
-		vim.api.nvim_set_hl(0, group, { bg = "none" })
-	end
-end
-
 return {
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
-
 		opts = {
-			style = "storm",
-			transparent = true,
+			style = "night",
+			transparent = false,
 		},
-
 		config = function(_, opts)
-			require("tokyonight").setup(opts)
-			vim.cmd.colorscheme("tokyonight")
-			enable_transparency()
+		require("tokyonight").setup(opts)
+		vim.cmd.colorscheme("tokyonight")
 		end,
 	},
-
 	{
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy",
-
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
 		},
-
 		opts = {
 			theme = "tokyonight",
 		},
